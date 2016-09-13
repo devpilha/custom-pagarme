5f626804bd95b236d8ffe0130c7eeab9eaf8b02c
Eduardo Nascimento <eduardo@eduardonascimento.com.br>
2016-09-13 17:17:56 -0300

    Remove unused method

---
+++ lib/PagarMe.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PagarMe\Sdk;
+
+use GuzzleHttp\Client as GuzzleClient;
+use PagarMe\Sdk\Customer\Handler as CustomerHandler;
+
+class PagarMe
+{
+    private $client;
+    private $customerHandler;
+
+    public function __construct($apiKey, $encryptionKey)
+    {
+        $this->client = new Client(
+            new GuzzleClient(['base_url' => 'https://api.pagar.me/1/']),
+            $apiKey,
+            $encryptionKey
+        );
+    }
+
+    public function customer()
+    {
+        if (!$this->customerHandler instanceof CustomerHandler) {
+            $this->customerHandler = new CustomerHandler($this->client);
+        }
+
+        return $this->customerHandler;
+    }
+}
