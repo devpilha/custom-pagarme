f2b11559c72fa96d80ec9cd280e9951821877596
Eduardo Nascimento <eduardo@eduardonascimento.com.br>
2016-09-29 14:49:57 -0300

    Add test to transaction handler

--- tests/unit/Transaction/HandlerTest.php
+++ tests/unit/Transaction/HandlerTest.php
@@ -39,6 +39,7 @@ class HandlerTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @test
     **/
     public function mustCreateBoletoTransaction()
     {
@@ -65,6 +66,7 @@ class HandlerTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @test
     **/
     public function mustReturnBoletoTransaction()
     {
@@ -87,6 +89,7 @@ class HandlerTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @test
     **/
     public function mustReturnCreditCardTransaction()
     {
@@ -109,6 +112,30 @@ class HandlerTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @test
+    **/
+    public function mustReturnRefundedTransaction()
+    {
+        $clientMock = $this->getMockBuilder('PagarMe\Sdk\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $clientMock->method('send')
+            ->willReturn(json_decode($this->creditCardTransactionRefundResponse()));
+
+        $handler = new Handler($clientMock);
+
+        $transactionMock = $this->getMockBuilder('PagarMe\Sdk\Transaction\Transaction')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $transactionMock->method('getId')->willReturn(rand(1000, 2000));
+
+        $transaction = $handler->refund($transactionMock);
+
+        $this->assertTrue($transaction->isRefunded());
+    }
+
+    /**
+     * @test
      * @expectedException PagarMe\Sdk\Transaction\UnsupportedTransaction
     **/
     public function mustThrowUnsupportedTransaction()
@@ -129,6 +156,7 @@ class HandlerTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @test
     **/
     public function mustReturnArrayOfTransaction()
     {
@@ -149,6 +177,11 @@ class HandlerTest extends \PHPUnit_Framework_TestCase
     public function creditCardTransactionCreateResponse()
     {
         return '{"object":"transaction","status":"processing","refuse_reason":null,"status_reason":"acquirer","acquirer_response_code":null,"authorization_code":null,"soft_descriptor":"testeDeAPI","tid":null,"nsu":null,"date_created":"2015-02-25T21:54:56.000Z","date_updated":"2015-02-25T21:54:56.000Z","amount":310000,"installments":5,"id":184220,"cost":0,"postback_url":"http://requestb.in/pkt7pgpk","payment_method":"credit_card","antifraud_score":null,"boleto_url":null,"boleto_barcode":null,"boleto_expiration_date":null,"referer":"api_key","ip":"189.8.94.42","subscription_id":null,"phone":null,"address":null,"customer":null,"card":{"object":"card","id":"card_ci6l9fx8f0042rt16rtb477gj","date_created":"2015-02-25T21:54:56.000Z","date_updated":"2015-02-25T21:54:56.000Z","brand":"mastercard","holder_name":"Api Customer","first_digits":"548045","last_digits":"3123","fingerprint":"HSiLJan2nqwn","valid":null},"metadata":{"idProduto":"13933139"}}';
+    }
+
+    public function creditCardTransactionRefundResponse()
+    {
+        return '{"object":"transaction","status":"refunded","refuse_reason":null,"status_reason":"acquirer","acquirer_response_code":"00","acquirer_name":"pagarme","authorization_code":"418519","soft_descriptor":null,"tid":742083,"nsu":742083,"date_created":"2016-09-29T15:36:08.020Z","date_updated":"2016-09-29T17:48:34.600Z","amount":10000,"authorized_amount":10000,"paid_amount":10000,"refunded_amount":10000,"installments":1,"id":742083,"cost":0,"card_holder_name":"João Silva","card_last_digits":"8357","card_first_digits":"491668","card_brand":"visa","postback_url":null,"payment_method":"credit_card","capture_method":"ecommerce","antifraud_score":null,"boleto_url":null,"boleto_barcode":null,"boleto_expiration_date":null,"referer":"api_key","ip":"187.11.121.49","subscription_id":null,"phone":{"object":"phone","ddi":"55","ddd":"11","number":"999887766","id":43754},"address":{"object":"address","street":"rua qualquer","complementary":"apto","street_number":"13","neighborhood":"pinheiros","city":"sao paulo","state":"SP","zipcode":"05444040","country":"Brasil","id":45774},"customer":{"object":"customer","document_number":"18152564000105","document_type":"cnpj","name":"nome do cliente","email":"user57ed355d4340c@email.com","born_at":"1970-01-01T03:38:41.988Z","gender":"M","date_created":"2016-09-13T18:04:17.200Z","id":93076},"card":{"object":"card","id":"card_citn4cmtp00u2dn6e82m7ovky","date_created":"2016-09-28T16:21:11.437Z","date_updated":"2016-09-28T16:21:11.595Z","brand":"visa","holder_name":"João Silva","first_digits":"491668","last_digits":"8357","country":"US","fingerprint":"rv00k8EENnuX","valid":true,"expiration_date":"0723"},"split_rules":null,"metadata":{},"antifraud_metadata":{}}';
     }
 
     public function transactionListResponse()
